e715388fae45d3a2f85752f0693b5710e2793abc
Brahim Timezghine <mzabbrah47@gmail.com>
2023-12-02 12:49:37 +0100

    Create RegisterPatientViewModel.cs

---
+++ ViewModels/RegisterPatientViewModel.cs
@@ -0,0 +1,12 @@
+public class RegisterPatientViewModel
+{
+    [Required]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+
+    // Add other patient-specific properties if needed
+}
