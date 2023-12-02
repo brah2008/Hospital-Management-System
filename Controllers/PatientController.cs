0a1fcd6e69d516cdcb13cead54eb80f0578df6fb
Brahim Timezghine <mzabbrah47@gmail.com>
2023-12-02 12:18:43 +0100

    Update PatientController.cs

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -1,26 +1,30 @@
 [Route("patients")]
 public class PatientController : Controller
 {
-    private readonly ApplicationDbContext _context;
-    public PatientController(ApplicationDbContext context)
+    private readonly PatientService _patientService;
+
+    public PatientController(PatientService patientService)
     {
-        _context = context;
+        _patientService = patientService;
     }
+
     [HttpGet]
     public IActionResult Index()
     {
-        var patients = _context.Patients.ToList();
+        var patients = _patientService.GetAllPatients();
         return View(patients);
     }
+
     [HttpGet("{id}")]
     public IActionResult Details(int id)
     {
-        var patient = _context.Patients.FirstOrDefault(p => p.PatientId == id);
+        var patient = _patientService.GetPatientById(id);
         if (patient == null)
         {
             return NotFound();
         }
         return View(patient);
     }
+
     // Other CRUD actions...
 }
