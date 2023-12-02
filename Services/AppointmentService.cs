958a0cfc026deec3d8626444aef131a30cc1c01f
Brahim Timezghine <mzabbrah47@gmail.com>
2023-12-02 11:23:02 +0100

    Create AppointmentService.cs

---
+++ Services/AppointmentService.cs
@@ -0,0 +1,40 @@
+public class AppointmentService
+{
+    private readonly ApplicationDbContext _context;
+    public AppointmentService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public List<Appointment> GetAllAppointments()
+    {
+        return _context.Appointments.ToList();
+    }
+
+    public Appointment GetAppointmentById(int id)
+    {
+        return _context.Appointments.FirstOrDefault(a => a.AppointmentId == id);
+    }
+
+    public void AddAppointment(Appointment appointment)
+    {
+        _context.Appointments.Add(appointment);
+        _context.SaveChanges();
+    }
+
+    public void UpdateAppointment(Appointment appointment)
+    {
+        _context.Appointments.Update(appointment);
+        _context.SaveChanges();
+    }
+
+    public void DeleteAppointment(int id)
+    {
+        var appointment = _context.Appointments.Find(id);
+        if (appointment != null)
+        {
+            _context.Appointments.Remove(appointment);
+            _context.SaveChanges();
+        }
+    }
+}
