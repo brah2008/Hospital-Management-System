cdf801c48169573312d99cdbdd428ed242e586d6
Brahim Timezghine <mzabbrah47@gmail.com>
2023-12-02 11:06:40 +0100

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -10,4 +10,12 @@ CREATE TABLE Doctors (
     LastName VARCHAR(50),
     -- Other columns...
 );
--- Other tables...
+CREATE TABLE Appointments (
+    AppointmentId INT PRIMARY KEY,
+    AppointmentDate DATETIME,
+    -- Other columns...
+    PatientId INT,
+    DoctorId INT,
+    FOREIGN KEY (PatientId) REFERENCES Patients(PatientId),
+    FOREIGN KEY (DoctorId) REFERENCES Doctors(DoctorId)
+);
