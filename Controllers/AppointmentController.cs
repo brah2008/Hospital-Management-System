f5283be993d29a7383066a35bbfd83565c13bc26
Brahim Timezghine <mzabbrah47@gmail.com>
2023-12-02 12:27:46 +0100

    Update AppointmentController.cs

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -3,15 +3,21 @@ public class AppointmentController : Controller
 {
     private readonly AppointmentService _appointmentService;
 
+    private readonly ApplicationDbContext _context;
+
     public AppointmentController(AppointmentService appointmentService)
     {
         _appointmentService = appointmentService;
+        _context = context;
     }
 
     [HttpGet]
     public IActionResult Index()
     {
-        var appointments = _appointmentService.GetAllAppointments();
+        //var appointments = _appointmentService.GetAllAppointments();
+        //return View(appointments);
+
+        var appointments = _context.Appointments.Include(a => a.Patient).Include(a => a.Doctor).ToList();
         return View(appointments);
     }
 
