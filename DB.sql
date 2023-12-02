a5da8b50044fabe1c157d6582314ee4cc49f97a4
Brahim Timezghine <mzabbrah47@gmail.com>
2023-12-02 10:45:39 +0100

    Create DB.sql

---
+++ DB.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Patients (
+    PatientId INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    -- Other columns...
+);
+CREATE TABLE Doctors (
+    DoctorId INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    -- Other columns...
+);
+-- Other tables...
