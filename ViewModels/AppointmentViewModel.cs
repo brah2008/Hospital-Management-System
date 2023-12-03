f1b7e51ff9b80e2b115d8f0a86e421bdd66ae49b
Brahim Timezghine <mzabbrah47@gmail.com>
2023-12-03 07:47:27 +0100

    Update AppointmentViewModel.cs

--- ViewModels/AppointmentViewModel.cs
+++ ViewModels/AppointmentViewModel.cs
@@ -1,8 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 public class AppointmentViewModel
 {
     [Required]
     [Display(Name = "Appointment Date")]
     public DateTime AppointmentDate { get; set; }
 
+    [Required]
+    [Display(Name = "Patient")]
+    public int PatientId { get; set; }
+
+    [Required]
+    [Display(Name = "Doctor")]
+    public int DoctorId { get; set; }
+
     // Add other appointment-specific properties if needed
+    // For example, additional notes, reason for appointment, etc.
 }
