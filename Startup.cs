99996d4039e4ef2d08906494319ed3f07bcf4b17
Brahim Timezghine <mzabbrah47@gmail.com>
2023-12-03 07:37:14 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,45 +1,50 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using System;
 
-public void ConfigureServices(IServiceCollection services)
+public class Startup
 {
-    // Dependency injection configuration
-    services.AddControllersWithViews();
-    // Database configuration
-    services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-    // Other services...
+    // ... (Existing code)
 
-    services.AddIdentity<IdentityUser, IdentityRole>()
-        .AddEntityFrameworkStores<ApplicationDbContext>()
-        .AddDefaultTokenProviders();
-
-    // Configure cookie authentication
-    services.ConfigureApplicationCookie(options =>
+    public void ConfigureServices(IServiceCollection services)
     {
-        options.Cookie.Name = "YourAppCookieName";
-        options.Cookie.HttpOnly = true;
-        options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
-        options.LoginPath = "/Account/Login";
-        options.AccessDeniedPath = "/Account/AccessDenied";
-        options.SlidingExpiration = true;
-    });
+        // Dependency injection configuration
+        services.AddControllersWithViews();
+        // Database configuration
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        // Other services...
 
-}
-public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-{
+        services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
 
-    // Middleware configuration
-    app.UseDeveloperExceptionPage();
-    app.UseHttpsRedirection();
-    app.UseStaticFiles();
-    app.UseAuthentication();
-    app.UseRouting();
-    app.UseAuthorization();
-    app.UseEndpoints(endpoints =>
+        // Configure cookie authentication
+        services.ConfigureApplicationCookie(options =>
+        {
+            options.Cookie.Name = "YourAppCookieName";
+            options.Cookie.HttpOnly = true;
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+            options.LoginPath = "/Account/Login";
+            options.AccessDeniedPath = "/Account/AccessDenied";
+            options.SlidingExpiration = true;
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        endpoints.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-    });
+        // Middleware configuration
+        app.UseDeveloperExceptionPage();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseAuthentication();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
 }
