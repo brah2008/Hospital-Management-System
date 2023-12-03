8743a9dded777c501b0a2682b2476e4bf90fef03
Brahim Timezghine <mzabbrah47@gmail.com>
2023-12-03 07:53:15 +0100

    Update RegisterPatientViewModel.cs

--- ViewModels/RegisterPatientViewModel.cs
+++ ViewModels/RegisterPatientViewModel.cs
@@ -1,12 +1,44 @@
-public class RegisterPatientViewModel
+[Authorize(Roles = "Admin, Nurse")]
+[Route("patients")]
+public class PatientController : Controller
 {
-    [Required]
-    [Display(Name = "First Name")]
-    public string FirstName { get; set; }
+    private readonly ApplicationDbContext _context;
 
-    [Required]
-    [Display(Name = "Last Name")]
-    public string LastName { get; set; }
+    public PatientController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
 
-    // Add other patient-specific properties if needed
+    [HttpGet("register")]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost("register")]
+    [ValidateAntiForgeryToken]
+    public IActionResult Register(RegisterPatientViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // Implement patient registration logic
+            var patient = new Patient
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                DateOfBirth = model.DateOfBirth
+                // Set other properties accordingly
+            };
+
+            _context.Patients.Add(patient);
+            _context.SaveChanges();
+
+            // Redirect to the appropriate page, e.g., patient details or list of patients
+            return RedirectToAction("Details", new { id = patient.PatientId });
+        }
+
+        return View(model);
+    }
+
+    // Add other patient-related actions (e.g., edit, delete, details) as needed
 }
