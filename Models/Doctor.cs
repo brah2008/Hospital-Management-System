156265a585e6f4b52757ca1c1ceea5133f346202
Brahim Timezghine <mzabbrah47@gmail.com>
2023-12-03 08:26:17 +0100

    Update Doctor.cs

--- Models/Doctor.cs
+++ Models/Doctor.cs
@@ -1,9 +1,17 @@
 public class Doctor
 {
     public int DoctorId { get; set; }
+
+    [Required]
+    [Display(Name = "First Name")]
     public string FirstName { get; set; }
+
+    [Required]
+    [Display(Name = "Last Name")]
     public string LastName { get; set; }
-    // Other properties...
+
+    // Add other properties as needed
+
     // Navigation properties
     public ICollection<Appointment> Appointments { get; set; }
 }
